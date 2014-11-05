00a03ff018ae5eb67d23f21be24b0a592182684f
Marcel Amado <ac.marcel@gmail.com>
2014-11-04 23:46:11 -0500

    Correcting validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -23,6 +23,19 @@ class ProductTest < ActiveSupport::TestCase
                       price: 1,
                       image_url: image_url)
   end
+
+  test "product price must be positive" do
+    product = Product.new(title: "My Book Title",
+                          description: "yyy",
+                          image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid? assert_equal ["must be greater than or equal to 0.01"],product.errors[:price]
+    product.price = 0
+    assert product.invalid? assert_equal ["must be greater than or equal to 0.01"],product.errors[:price]
+    product.price = 1
+    assert product.valid?
+  end
+
   test "image url" do
     ok= %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif}
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
@@ -40,6 +53,6 @@ class ProductTest < ActiveSupport::TestCase
                           price: 1,
                           image_url:"fred.gif")
     assert product.invalid?
-    assert_equal [I18n.translate('errors.messages.taken')],products.errors[:title]
+    assert_equal [I18n.translate('errors.messages.taken')],product.errors[:title]
   end
 end
